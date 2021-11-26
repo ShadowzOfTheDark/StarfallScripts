ebe2cca99bdec13729390fdadea5a6cb0a089776
ShadowzOfTheDark <57125303+ShadowzOfTheDark@users.noreply.github.com>
2021-11-25 21:09:40 -0600

    Update command_proxy.lua

--- command_proxy.lua
+++ command_proxy.lua
@@ -1,6 +1,8 @@
 --Only a little sus
 
 if commands ~= nil then
+	term.clear()
+	term.setCursorPos(1,1)
 	while true do
 		os.sleep(0.5)
 		local input = fs.open("cmd_proxy/input.txt","r")
@@ -11,6 +13,12 @@ if commands ~= nil then
 				print("Command: "..contents)
 				input = fs.open("cmd_proxy/input.txt","w")
 				input.close()
+				output = fs.open("cmd_proxy/output.txt","w")
+				local exec, result = commands.exec(contents)
+				for _,str in ipairs(result) do
+					result.write(result)
+				end
+				output.close()
 			else
 			
 			end
